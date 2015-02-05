6088ddcb83071aaeabd1d16dffd0a4f204e49064
gingyneil <neil.reading.nr@gmail.com>
2015-02-05 18:02:58 -0500

    Added randomPower method

--- Assignment2/SuperHero.cs
+++ Assignment2/SuperHero.cs
@@ -8,18 +8,61 @@ namespace Assignment2
 {
     class SuperHero : Hero
     {
+       string[] superPowers = { "Super	Speed", "Super	Strength", "Body Armour", "Flight", "Fire Generation", "Weather Control" };
+       string[] newArray = new string[3];
+       int index =0;
 
         public SuperHero(string name) : base(name)
         {
-        
+            generateRandomPowers();
         
         }
-    
-    
-    
-    
-    
-    
+
+
+
+
+
+
+
+        public void generateRandomPowers()
+        {
+           Random rnd = new Random();
+       
+            while (index < newArray.Length)
+            {
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            }
+       
+       }
+
+
+
+
+
+
+
+
+
+
+        private static int generateRandomPowerMember(Random rnd) 
+        {
+            int randomPower;
+
+            randomPower = rnd.Next(6);
+            Console.WriteLine("My Randome power is {0}", randomPower);
+            return randomPower;
+        }
+
+
     
     }
 }
